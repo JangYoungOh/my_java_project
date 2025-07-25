b2ccb33068e33a74f6146e63d7fadb935c90ba2b
bluesky <bluesky@naver.com>
2025-07-25 11:59:51 +0900

    진짜 첫번째 연습

---
+++ src/ch02_control_statement/If01.java
@@ -0,0 +1,15 @@
+package ch02_control_statement;
+
+public class If01 {
+    public static void main(String[] args) {
+        int su = 10;
+
+        if (su%2==0){
+            System.out.println("숫자" + su +"은(는) 짝수입니다.");
+
+        }else {
+            System.out.println("숫자" + su +"은(는) 홀수입니다.");
+
+        }
+    }
+}
